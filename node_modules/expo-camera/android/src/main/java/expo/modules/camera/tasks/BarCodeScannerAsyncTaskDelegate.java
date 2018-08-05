0280f3bcece85a895cd62bda4fb4993f86287a4c
dulalsaurab <dulal.saurab@gmail.com>
2018-08-05 10:09:24 -0500

    ignoring node files

--- node_modules/expo-camera/android/src/main/java/expo/modules/camera/tasks/BarCodeScannerAsyncTaskDelegate.java
+++
@@ -1,8 +0,0 @@
-package expo.modules.camera.tasks;
-
-import expo.modules.camera.utils.ExpoBarCodeDetector;
-
-public interface BarCodeScannerAsyncTaskDelegate {
-  void onBarCodeRead(ExpoBarCodeDetector.Result barCode);
-  void onBarCodeScanningTaskCompleted();
-}
